d8631fd2db89ab622095c8dd68f1d51649c92004
Martyna <martyna.szczekocka@gmail.com>
2022-04-08 21:16:04 +0200

    x, y

--- main.cpp
+++ main.cpp
@@ -7,13 +7,34 @@ int main()
 {
 	setlocale(LC_ALL, "Polish");
 	Simulation *sim = new SimpleSimulation;
-    Point p1 = Point(3,4);
-    Point p2 = Point(8,14);
-    std::cout << "x: " << p1.x << " y: " << p1.y << std::endl;
-    Vector2D v1 = Vector2D(p1, p2);
 
+    Vector2D v1 = Vector2D(2.5, 4);
+    Vector2D v2 = Vector2D(8);
+    Vector2D v3 = Vector2D();
 
+    v1.Show();
+    v2.Show();
+    v3.Show();
 
+    -v1;
+    v1.Show();
+    -v1;
+
+    Vector2D v4 = v1 - v2;
+    v4.Show();
+
+    v4 += v1;
+    v4.Show();
+
+    Vector2D v5 = v1 * 5;
+    v5.Show();
+
+    std::cout << "v5 length: " << v5.vLength() << std::endl;
+
+    Vector2D v6 = v1.normalize();
+    cout << "v6 length: " << v6.vLength() << std::endl;
+
+    std::cout << v1.dist(v2) << std::endl;
 
 
 
